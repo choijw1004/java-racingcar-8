7a6ec79d86b4cdead66ebf8987c6125bc4fc35c4
choijw1004 <97choijw@gmail.com>
2025-10-25 18:09:25 +0900

    feat: InputView 기본 구조 생성 - 안내 문구를 형식에 맞게 출력한다. - 입력받은 문자열을 ,로 분리하여 이를 List로 반환한다.

---
+++ src/main/java/racingcar/View/InputView.java
@@ -0,0 +1,26 @@
+package racingcar.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+
+    private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPT_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String DELIMITER = ",";
+
+    public List<String> readCarNames() {
+        System.out.println(CAR_NAMES_INPUT_MESSAGE);
+        String input = Console.readLine();
+
+        return splitByDelimiter(input);
+    }
+
+    private List<String> splitByDelimiter(String input){
+        String[] carNames = input.split(DELIMITER);
+
+        return Arrays.asList(carNames);
+    }
+}
