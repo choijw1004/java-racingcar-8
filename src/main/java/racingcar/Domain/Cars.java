63b7ddf6929ca4494f029866bacaf6ed0c632171
choijw1004 <97choijw@gmail.com>
2025-10-25 15:05:36 +0900

    feat: 쉼표로 구분된 이름 목록으로 여러 대의 자동차를 생성한다. - 각 이름의 앞 뒤 공백은 trim()으로 제거한다. - 중복된 이름이 포함되어 있으면 에러를 발생시킨다.

---
+++ src/main/java/racingcar/Domain/Cars.java
@@ -0,0 +1,54 @@
+package racingcar.Domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Cars {
+    private static final int MOVE_CONDITION = 4;
+    private static final int MIN_RANDOM = 0;
+    private static final int MAX_RANDOM = 9;
+
+    private final List<Car> cars;
+
+    public Cars(List<String> names) {
+        if (hasDuplicateName(names)) {
+            throw new IllegalArgumentException("중복된 이름이 있습니다.");
+        }
+
+        this.cars = createCars(names);
+    }
+
+    private List<Car> createCars(List<String> names) {
+        List<Car> carList = new ArrayList<>();
+
+        for (String name : names) {
+            carList.add(new Car(name.trim()));
+        }
+
+        return carList;
+    }
+
+    private boolean hasDuplicateName(List<String> names) {
+        List<String> trimNameList = makeTrimNameList(names);
+        HashSet<String> uniqueNameSet = new HashSet<>(trimNameList);
+
+        if (trimNameList.size() != uniqueNameSet.size()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private List<String> makeTrimNameList(List<String> names) {
+        List<String> trimNameList = new ArrayList<>();
+
+        for (String name : names) {
+            trimNameList.add(name.trim());
+        }
+
+        return trimNameList;
+    }
+
+
+}
