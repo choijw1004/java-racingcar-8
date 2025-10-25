d6c0a9fc3ddb3bc24776dd4e6e26a65ebf0fef8d
choijw1004 <97choijw@gmail.com>
2025-10-25 16:15:23 +0900

    feat: RacingGame 클래스 클래스 구현 - 자동차 목록과 시도 횟수로 게임을 생성한다. - 시도 횟수는 1 이상이어야 한다. - 시도 횟수가 1 미만이면 에러를 발생시킨다. - 한 라운드를 진행하고 자동차들을 이동 시킨다. - 우승자 목록을 반환한다.

---
+++ src/main/java/racingcar/Domain/RacingGame.java
@@ -0,0 +1,38 @@
+package racingcar.Domain;
+
+import java.util.List;
+
+public class RacingGame {
+    private static final int MIN_ATTEMPT_COUNT = 1;
+
+    private final Cars cars;
+    private final int attemptCount;
+
+    public RacingGame(Cars cars, int attemptCount) {
+        validateAttemptCount(attemptCount);
+        this.cars = cars;
+        this.attemptCount = attemptCount;
+    }
+
+    private void validateAttemptCount(int attemptCount) {
+        if (attemptCount < MIN_ATTEMPT_COUNT) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+    }
+
+    public void startRacing() {
+        cars.moveCars();
+    }
+
+    public List<String> getCurrentStatusStringList() {
+        return cars.getCurrentStatusStringList();
+    }
+
+    public List<String> getWinners() {
+        return cars.getWinners();
+    }
+
+    public int getAttemptCount() {
+        return attemptCount;
+    }
+}
