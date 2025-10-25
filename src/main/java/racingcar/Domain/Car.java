1c322f2d7cc136a46902d4a476b698d1b2f21998
choijw1004 <97choijw@gmail.com>
2025-10-25 14:15:28 +0900

    feat: 자동차 기본 구조 생성 - 자동차는 이름과 위치를 가진다. - 이름은 1자 이상, 5자 이하만 가능하다 - 이름이 빈 문자열이면 에러를 발생시킨다. - 이름이 5자를 초과하면 에러를 발생시킨다. - 자동차의 시작 위치는 0

---
+++ src/main/java/racingcar/Domain/Car.java
@@ -0,0 +1,27 @@
+package racingcar.Domain;
+
+public class Car {
+    private static final int START_POSITION = 0;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String POSITION = "-";
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        isValidateName(name);
+        this.name = name;
+        this.position = START_POSITION;
+    }
+
+    private void isValidateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈 문자열일 수 없습니다.");
+        }
+
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+        }
+    }
+}
+
