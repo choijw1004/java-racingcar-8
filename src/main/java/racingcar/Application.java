2cdbd2476e52d39e4aa85cb6774c073119ee880f
choijw1004 <97choijw@gmail.com>
2025-10-25 18:41:20 +0900

    feat: Application 클래스 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        try {
+            Controller controller = new Controller();
+            controller.run();
+        } finally {
+            Console.close();
+        }
     }
 }
