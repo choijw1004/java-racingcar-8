c8e9353233633657d5cd758f1ecc945c31bf3f90
choijw1004 <97choijw@gmail.com>
2025-10-27 00:25:06 +0900

    test: 통합 테스트 추가 - 여러번의 시도(Attempt)가 있는 경우 - 공동 우승자가 있는 경우 - 세 명이상 공동 우승이 있는 경우

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,41 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 여러_번_시도_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("choi,jang,woo", "5");
+                    assertThat(output()).contains("choi :", "jang :", "woo :", "최종 우승자 :");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP,
+                MOVING_FORWARD, MOVING_FORWARD, STOP, STOP, MOVING_FORWARD,
+                MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 공동_우승자_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("choi,jang", "1");
+                    assertThat(output()).contains("choi : -", "jang : -", "최종 우승자 : choi,jang");  // 공백 제거
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 세_명_이상_공동_우승_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("choi,jang,woo", "1");
+                    assertThat(output()).contains("최종 우승자 : choi,jang,woo");  // 공백 제거
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
