89a2e896d8f067ba4debb63e9535b2a319dc5cc1
choijw1004 <97choijw@gmail.com>
2025-10-26 23:33:03 +0900

    refactor: controller 패키지 및 Controller 클래스 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,19 +1,48 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.controller.Controller;
+import racingcar.domain.Cars;
+import racingcar.domain.RacingGame;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
-public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+import java.util.List;
 
+public class Application {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Application() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public static void main(String[] args) {
         try {
-            Controller controller = new Controller(new InputView(), new OutputView());
-            controller.run();
+            new Application().run();
         } finally {
             Console.close();
         }
+    }
+
+    public void run() {
+        List<String> carNames = inputView.readCarNames();
+        int attemptCount = inputView.readAttemptCount();
+
+        Cars cars = new Cars(carNames);
+        RacingGame racingGame = new RacingGame(cars, attemptCount);
+
+        play(racingGame);
+    }
+
+    private void play(RacingGame game) {
+        outputView.printResultMessage();
+
+        for (int attempt = 0; attempt < game.getAttemptCount(); attempt++) {
+            game.startRacing();
+            outputView.printAttemptResult(game.getStatusDtos());
+        }
+
+        outputView.printWinners(game.getWinners());
     }
 }
